8ad21212ec5e86504777e8d5c183f5ea33a47b75
luizdcastro <toluizcastro@gmail.com>
2025-09-21 21:25:12 +0200

    Add default payload and CORS middleware for development

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
     "log"
     "os"
+    "time"
 
+    "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
     "github.com/joho/godotenv"
     "payrails-server-demo/controllers"
@@ -15,6 +17,15 @@ func main() {
     }
 
     r := gin.Default()
+
+    r.Use(cors.New(cors.Config{
+        AllowAllOrigins:  true,
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge: 12 * time.Hour,
+    }))
 
     r.POST("/payrails/sdk", controllers.InitSDK)
 
