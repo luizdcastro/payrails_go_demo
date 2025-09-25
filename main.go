c5cf6a5fb6e707b84aded31a1e5cf2af42c237b0
luizdcastro <toluizcastro@gmail.com>
2025-09-25 16:15:07 +0200

    Fix CORS config: allow x-client-key header

--- main.go
+++ main.go
@@ -18,14 +18,14 @@ func main() {
 
     r := gin.Default()
 
-    r.Use(cors.New(cors.Config{
-        AllowAllOrigins:  true,
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-        MaxAge: 12 * time.Hour,
-    }))
+ r.Use(cors.New(cors.Config{
+    AllowAllOrigins:  true,
+    AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "x-client-key"},
+    ExposeHeaders:    []string{"Content-Length"},
+    AllowCredentials: true,
+    MaxAge: 12 * time.Hour,
+}))
 
     r.POST("/payrails/sdk", controllers.InitSDK)
 
